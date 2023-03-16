fea5c09831055c3fed6902a9d3d82ee799f16c04
yiminghuang47 <yiminghuang47@gmail.com>
2023-03-16 17:54:40 -0400

    minor fixes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:3000/",
+    origin: process.env.REACT_APP_API_URL,
     credentials: true,
   })
 );
